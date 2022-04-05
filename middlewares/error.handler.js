d083b66c7719f7f20a0a121cf8e693997a2bfe4a
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-04-04 20:51:37 -0500

    capturando errores de ORM con middleware

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,3 +1,5 @@
+const { ValidationError } = require("sequelize/types");
+
 function logErrors (err, req, res, next) {
   console.error(err);
   next(err);
@@ -10,6 +12,17 @@ function errorHandler(err, req, res, next) {
   });
 }
 
+function ormErrorHandler(err, req, res, next) {
+  if(err instanceof ValidationError){
+    res.status(409).json({
+      statusCode:409,
+      message: err.message,
+      errors: err.errors,
+    });
+  }
+  next(err)
+}
+
 function boomErrorHandler(err, req, res, next) {
   if (err.isBoom) {
     const { output } = err;
@@ -19,4 +32,4 @@ function boomErrorHandler(err, req, res, next) {
 }
 
 
-module.exports = { logErrors, errorHandler, boomErrorHandler }
+module.exports = { logErrors, errorHandler, boomErrorHandler, ormErrorHandler }

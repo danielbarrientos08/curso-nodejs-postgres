0b5f5da2e41008489539f8abc2c9c058bb41b1ed
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-04-05 22:01:39 -0500

    modificando migraciones

--- db/migrations/20220405215003-create-order-table.js
+++ db/migrations/20220405215003-create-order-table.js
@@ -1,11 +1,39 @@
 'use strict';
 
-const { OrderSchema, ORDER_TABLE} =require('./../models/order.model')
+const {ORDER_TABLE} =require('./../models/order.model')
+const {DataTypes,Sequelize} =require('sequelize')
+const { CUSTOMER_TABLE } = require('./../models/customer.model')
+
 
 module.exports = {
   async up (queryInterface) {
 
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema)
+    await queryInterface.createTable(ORDER_TABLE,  {
+
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      customerId:{
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        field: 'customer_id',
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+    })
   },
 
   async down (queryInterface) {

72e15a23ec13cb0a1ba4d61029b1acc060c601ae
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-04-05 20:57:21 -0500

    configurando para deploy

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -3,16 +3,22 @@ const { Sequelize } = require('sequelize')
 const { config } = require('./../config/config')
 const  setupModels  = require('./../db/models')
 
-const USER = encodeURIComponent(config.dbUser)
-const PASSWORD = encodeURIComponent(config.dbPassword)
+// const USER = encodeURIComponent(config.dbUser)
+// const PASSWORD = encodeURIComponent(config.dbPassword)
 
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
-
-
-const sequelize = new Sequelize(URI, {
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const options = {
   dialect: 'postgres',
-  logging: true,
-})
+  logging: config.isProd ? false:true,//ver o no errores de base de datos
+}
+
+if(config.isProd){
+  options.ssl = {
+    rejectUnauthorized:false
+  }
+}
+
+const sequelize = new Sequelize(config.dbUrl, options)
 
 setupModels(sequelize)
 
